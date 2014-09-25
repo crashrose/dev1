b9a5a7fd15303c51335ccb6c00b635476dcc9099
crashrose <crashrose@gmail.com>
2014-09-25 15:13:51 -0400

    changing route for proces_ipn to GET instead of POST

--- config/routes.rb
+++ config/routes.rb
@@ -26,9 +26,13 @@ post '/admin/playbooks/show_params',
   to: 'admin/playbooks#show_params',
   as: 'show_params'
 
-post '/process_ipn',
+get '/process_ipn',
   to: 'payment_notifications#process_ipn',
   as: 'process_ipn'
+
+# post '/process_ipn',
+#   to: 'payment_notifications#process_ipn',
+#   as: 'process_ipn'
 
   resource :calendar, :only => [:show]
   resources :g_cal_events do
