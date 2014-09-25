d3ada693206bb6946bd4464dc1c3f03eea66297f
crashrose <crashrose@gmail.com>
2014-09-25 15:41:08 -0400

    Updating IPN Listener - should currently show all params passed

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -20,18 +20,28 @@ class PaymentNotificationsController < ApplicationController
   end
 
   def process_ipn
-    notify_params = payment_notification_params
+    # notify_params = payment_notification_params
+        respond_to do |format|
+          format.html { render :json => params.to_json}
+          format.json { render :json => params.to_json}
+        end
 
-        PaymentNotification.create!(:params => notify_params,
-          :status => notify_params[:payment_status],
-          :transaction_id => notify_params[:txn_id],
-          :user_id => notify_params[:custom][:user_id],
-          :payment_id => notify_params[:custom][:payment_id]
-        )
-    render :nothing => true
+        # PaymentNotification.create!(:params => notify_params,
+        #   :status => notify_params[:payment_status],
+        #   :transaction_id => notify_params[:txn_id],
+        #   :user_id => notify_params[:custom][:user_id],
+        #   :payment_id => notify_params[:custom][:payment_id]
+        # )
+
+    # render params.to_json
+  end
+
+  def ipn_params
+      params.require(:custom).permit(:user_id, :org_id, :payment_id)
+      params.require(:txn_id, :payment_status)
   end
 
   def payment_notification_params
-      params.require(:params).permit!
+      params.require(:payment_notification).permit(:custom) #shouldn't currently work
   end
 end
