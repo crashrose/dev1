0168e8379d844ca3fd052fc45ecd372372aee164
crashrose <crashrose@gmail.com>
2014-09-25 13:41:33 -0400

    trying again with active admin assets removed from precompile since they're already precompiled.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile =  ['*.js', '*.css'] 
+  config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
